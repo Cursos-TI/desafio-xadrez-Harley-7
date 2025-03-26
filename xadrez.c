3a32c30956a99b628d1b1b10c8d4420a780a53e3
Harley-7 <harleyoliveira008@gmail.com>
2025-03-26 20:32:01 +0000

    desafio aventureiro concluído

--- xadrez.c
+++ xadrez.c
@@ -5,8 +5,10 @@ int main() {
     int movimentos_da_torre = 1; //Torre 
     int movimentos_do_bispo = 1; //Bispo
     int movimentos_da_rainha =1; //Rainha
+    int movimento_vertical_cavalo;//Cavalo
+    int movimento_horizontal_cavalo = 1;//Cavalo
 
-    //Estrutura de repetição while reposável pelos movimentos da Torre
+    //Estrutura de repetição while reponsável pelos movimentos da Torre
     while(movimentos_da_torre <= 5){
 
         //Estrutura condicional para caso seja apenas um movimento a ação fique no singular caso seja mais de um será no plural
@@ -22,7 +24,7 @@ int main() {
 
     printf("\n");
 
-    //Estrutura de repetição do-while resposável pelos movimetos do Bispo
+    //Estrutura de repetição do-while responsável pelos movimetos do Bispo
     do{
 
        //Estrutura condicional para caso seja apenas um movimento a ação fique no singular caso seja mais de um será no plural
@@ -38,7 +40,7 @@ int main() {
 
     printf("\n");
 
-    //
+    //Estrutura de repetição for responsável pelos movimemtos da Rainha
     for ( movimentos_da_rainha; movimentos_da_rainha <= 8; movimentos_da_rainha++)
     {
         //Estrutura condicional para caso seja apenas um movimento a ação fique no singular caso seja mais de um será no plural
@@ -47,8 +49,31 @@ int main() {
         }else{
             printf("movimento feito pela Rainha: %d casas para esquerda\n", movimentos_da_rainha);
         }    
+        
     }
     
+    printf("\n");
+
+    //Estruturas de repetição aninhadas for e while responsáveis pelos movimentos do Cavalo
+    for(movimento_vertical_cavalo = 1; movimento_vertical_cavalo <= 2; movimento_vertical_cavalo++){
+
+        //Estrutura condicional para caso seja apenas um movimento a ação fique no singular caso seja mais de um será no plural
+        if(movimento_vertical_cavalo == 1)
+        {
+            printf("movimento feito pelo cavalo: %d casa para cima\n", movimento_vertical_cavalo);
+        }else{
+            printf("movimento feito pelo cavalo: %d casas para cima\n", movimento_vertical_cavalo);
+        }
+        
+        if(movimento_vertical_cavalo == 2){
+            while (movimento_horizontal_cavalo < 2)
+            {
+                printf("movimento feito pelo cavalo: 1 casa para direita\n"); 
+                movimento_horizontal_cavalo++;
+            }
+        }
+    }
+
     printf("\n");
 
     return 0;
