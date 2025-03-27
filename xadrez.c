e6c906b663f681133915a181ecc18be75857a32f
Harley-7 <harleyoliveira008@gmail.com>
2025-03-27 12:50:15 +0000

    desafio mestre concluído

--- xadrez.c
+++ xadrez.c
@@ -1,78 +1,99 @@
 #include <stdio.h>
 
-int main() {
-    //variáveis que serão usadas como condição para as estruturas de repetição
-    int movimentos_da_torre = 1; //Torre 
-    int movimentos_do_bispo = 1; //Bispo
-    int movimentos_da_rainha =1; //Rainha
-    int movimento_vertical_cavalo;//Cavalo
-    int movimento_horizontal_cavalo = 1;//Cavalo
 
-    //Estrutura de repetição while reponsável pelos movimentos da Torre
-    while(movimentos_da_torre <= 5){
+void movimento_torre(int movimentos_da_torre){
 
-        //Estrutura condicional para caso seja apenas um movimento a ação fique no singular caso seja mais de um será no plural
-        if(movimentos_da_torre == 1){
-            printf("movimento feito pela Torre: %d casa para a direita\n", movimentos_da_torre);
-            movimentos_da_torre++;
-        }else{
-            printf("movimento feito pela Torre: %d casas para a direita\n", movimentos_da_torre);
-            movimentos_da_torre++;
-        }
+    //Estrutura de repetição while responsável pelos movimentos da Torre
+    while(movimentos_da_torre >= 1){
+
+            printf("movimento feito pela Torre: direita\n", movimentos_da_torre);
+            movimentos_da_torre--;
         
     }
 
-    printf("\n");
+}
 
-    //Estrutura de repetição do-while responsável pelos movimetos do Bispo
+void movimento_bispo(int movimentos_do_bispo){
+
+    //Estrutura aninhada com do-while e for responsáveis pelos movimentos do Bispo
     do{
+        printf("movimento feito pelo Bispo: cima ");
 
-       //Estrutura condicional para caso seja apenas um movimento a ação fique no singular caso seja mais de um será no plural
-       if(movimentos_do_bispo == 1){
-            printf("movimento feito pelo Bispo: %d casa para cima direita\n", movimentos_do_bispo);
-            movimentos_do_bispo++;
-       }else{
-            printf("movimento feito pelo Bispo: %d casas para cima direita\n", movimentos_do_bispo);
-            movimentos_do_bispo++;
-       } 
+        for(int i = 5; i >= 1; i--){
+            if(movimentos_do_bispo == i){
+                printf("direita\n");
+                break;
+            }
+        }
 
-    }while (movimentos_do_bispo <= 5);
+        movimentos_do_bispo--;
+    }while(movimentos_do_bispo >= 1);
 
-    printf("\n");
+}
 
-    //Estrutura de repetição for responsável pelos movimemtos da Rainha
-    for ( movimentos_da_rainha; movimentos_da_rainha <= 8; movimentos_da_rainha++)
+void movimento_rainha(int movimentos_da_rainha){
+
+    //Estrutura de repetição for responsável pelos movimentos da Rainha 
+    for( movimentos_da_rainha; movimentos_da_rainha >= 1; movimentos_da_rainha--)
     {
-        //Estrutura condicional para caso seja apenas um movimento a ação fique no singular caso seja mais de um será no plural
-        if(movimentos_da_rainha == 1){
-            printf("movimento feito pela Rainha: %d casa para esquerda\n", movimentos_da_rainha);
-        }else{
-            printf("movimento feito pela Rainha: %d casas para esquerda\n", movimentos_da_rainha);
-        }    
+        
+            printf("movimento feito pela Rainha: esquerda\n");
         
     }
-    
-    printf("\n");
+
+}
+
+void movimento_cavalo(int movimentos_vertical_cavalo, int movimento_horizontal_cavalo){
 
     //Estruturas de repetição aninhadas for e while responsáveis pelos movimentos do Cavalo
-    for(movimento_vertical_cavalo = 1; movimento_vertical_cavalo <= 2; movimento_vertical_cavalo++){
+    for(movimentos_vertical_cavalo; movimentos_vertical_cavalo >= 1; movimentos_vertical_cavalo--){
 
-        //Estrutura condicional para caso seja apenas um movimento a ação fique no singular caso seja mais de um será no plural
-        if(movimento_vertical_cavalo == 1)
-        {
-            printf("movimento feito pelo cavalo: %d casa para cima\n", movimento_vertical_cavalo);
-        }else{
-            printf("movimento feito pelo cavalo: %d casas para cima\n", movimento_vertical_cavalo);
-        }
-        
-        if(movimento_vertical_cavalo == 2){
+            printf("movimento feito pelo cavalo: cima\n");
+       
+        if(movimentos_vertical_cavalo == 1){
             while (movimento_horizontal_cavalo < 2)
             {
-                printf("movimento feito pelo cavalo: 1 casa para direita\n"); 
+                printf("movimento feito pelo cavalo: direita\n"); 
                 movimento_horizontal_cavalo++;
             }
         }
     }
+
+}
+
+void movimento_cavalo(int movimentos_vertical_cavalo, int movimento_horizontal_cavalo){
+
+    //Estruturas de repetição aninhadas for e while responsáveis pelos movimentos do Cavalo
+    for(movimentos_vertical_cavalo; movimentos_vertical_cavalo >= 1; movimentos_vertical_cavalo--){
+
+            printf("movimento feito pelo cavalo: cima\n");
+       
+        if(movimentos_vertical_cavalo == 1){
+            while (movimento_horizontal_cavalo < 2)
+            {
+                printf("movimento feito pelo cavalo: direita\n"); 
+                movimento_horizontal_cavalo++;
+            }
+        }
+    }
+
+}
+
+int main() {
+    
+    movimento_torre(5);//recursividade da Torre
+
+    printf("\n");
+
+    movimento_bispo(5);//recursividade do Bispo
+
+    printf("\n");
+
+    movimento_rainha(8);//recursividade da Rainha
+    
+    printf("\n");
+
+    movimento_cavalo(2, 1);//recursividade do Cavalo
 
     printf("\n");
 
